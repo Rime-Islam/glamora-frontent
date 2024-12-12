f67e435335f16bd4dd29babade7bb93a98c3e632
rime-islam <rimeislam672@gmail.com>
2024-12-13 01:08:29 +0600

    redux provider's added

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -7,31 +7,32 @@ interface IAuthState {
     role: string;
   } | null;
   token: string | null;
-};
+}
 
+// Define the initial state using that type
 const initialState: IAuthState = {
   user: null,
   token: null,
 };
 
 export const authSlice = createSlice({
-  name: "authSlice",
-
-  initialState,
-  reducers: {
-    saveUserInfo: (
-      state,
-      action: PayloadAction<{
-        user: { email: string; role: string };
-        token: string;
-      }>
-    ) => {
-      state.token = action.payload.token;
-      state.user = action.payload.user;
+    name: "authSlice",
+  
+    initialState,
+    reducers: {
+      saveUserInfo: (
+        state,
+        action: PayloadAction<{
+          user: { email: string; role: string };
+          token: string;
+        }>
+      ) => {
+        state.token = action.payload.token;
+        state.user = action.payload.user;
+      },
     },
-  },
-});
-
-export const { saveUserInfo } = authSlice.actions;
-
-export default authSlice.reducer;
+  });
+  
+  export const { saveUserInfo } = authSlice.actions;
+  
+  export default authSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import cartSliceReducer from "@/redux/features/cart/cartSlice";
+import compareSliceReducer from "@/redux/features/compare/compareSlice";
+export const store = configureStore({
+  reducer: { cartSlice: cartSliceReducer, compareSlice: compareSliceReducer },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
